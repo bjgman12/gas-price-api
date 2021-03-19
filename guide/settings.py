056938c8f6c887b7f3a5d4d2afc392f7c02a4d90
bjgman12 <bjgman38@gmail.com>
2021-03-18 19:20:35 -0700

    local non docker fix

--- guide/settings.py
+++ guide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1ay=k&pwt-&ir#jc6!bl1aue4!a1%aw(jrb+iuuxiyw!xow-6h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
 
 
 # Application definition
